3ee4015bea4f43cc0c65ce143344f916ebdedb8f
Rishi Senthil Kumar <135209607+Rish1SK@users.noreply.github.com>
2025-10-23 12:12:48 -0400

    Refactor App component and update routing

--- proj2/frontend/pack-eats/src/App.js
+++ proj2/frontend/pack-eats/src/App.js
@@ -1,34 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/customer/Register";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
+import Browse from "./pages/customer/Browse";
+import Quiz from "./pages/customer/Quiz";
 
-function App() {
+const App = () => {
+  const [page, setPage] = useState("browse"); // 'browse' or 'quiz'
+
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-      <Router>
-        <Routes>
-            <Route path = "/register" element={<Register />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        {/* Register route */}
+        <Route path="/register" element={<Register />} />
+
+        {/* Home route */}
+        <Route
+          path="/"
+          element={
+            <div>
+              {page === "browse" && <Browse onTakeQuiz={() => setPage("quiz")} />}
+              {page === "quiz" && <Quiz onReturnToBrowse={() => setPage("browse")} />}
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
