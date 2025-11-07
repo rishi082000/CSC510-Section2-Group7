c811cd0e21859f727466d406355c420c728b4bed
MarkBits8 <priyaldoctor12@gmail.com>
2025-11-06 22:05:27 -0500

    Change DriverRepository ID type from Integer to UUID

--- proj2/backend/pack-eats/src/main/java/com/project/packEats/repository/DriverRepository.java
+++ proj2/backend/pack-eats/src/main/java/com/project/packEats/repository/DriverRepository.java
@@ -1,6 +1,7 @@
 package com.project.packEats.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,6 @@ import org.springframework.stereotype.Repository;
 import com.project.packEats.entity.driver.DriverEntity;
 
 @Repository
-public interface DriverRepository extends JpaRepository<DriverEntity, Integer> {
+public interface DriverRepository extends JpaRepository<DriverEntity, UUID> {
     Optional<DriverEntity> findByEmail(String email);
 }
